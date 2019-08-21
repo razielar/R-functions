7e13659e6851ed021fadb58dd1ee21fe8ea96206
razielar <razielar@gmail.com>
2019-08-21 18:58:05 +0200

    Euclidean distance Kmeans centroid

---
+++ ERC_data/f.Euclidean.distance.centroid.Kmeans.cutoff.R
@@ -0,0 +1,37 @@
+#### Function to calculate the euclidean distance between the K-mean
+#### centroid and the genes within a determined cluster 
+#August 21st 2019
+
+f.Centroid.Euclidean.dist <- function(Input_matrix){
+  
+  require(dplyr)
+  
+  Input_matrix <- as.data.frame(Input_matrix)
+  Input_matrix$Dist <- NA
+  mean_PC1 <- mean(Input_matrix$PC1)
+  mean_PC2 <- mean(Input_matrix$PC2)
+  
+  row_name <- rownames(Input_matrix)
+  
+  for(i in 1:nrow(Input_matrix)){
+    
+    gene_coor <- as.numeric(Input_matrix[i,1:2] )
+    matrix_gene <- rbind(gene_coor, c(mean_PC1, mean_PC2))
+    matrix_gene <- as.numeric(dist(matrix_gene, method = "euclidean") )
+    Input_matrix[i,5] <- matrix_gene
+    
+  }
+  
+  return(as.data.frame(Input_matrix))
+  
+}
+
+
+
+
+
+
+
+
+
+
