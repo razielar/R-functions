1cd55f45f9165346a5c39be1d55e194cc23bb756
razielar <razielar@gmail.com>
2019-08-27 12:13:15 +0200

    Add the function

--- modENCODE_data/f.Euclidean.distance.centroid.Kmeans.cutoff.R
+++ modENCODE_data/f.Euclidean.distance.centroid.Kmeans.cutoff.R
@@ -16,15 +16,31 @@ f.Centroid.Euclidean.dist <- function(Input_matrix){
   require(dplyr)
   require(magrittr)
   
+  Input_matrix <- as.data.frame(Input_matrix)
+  Input_matrix$Dist <- NA
+  mean_PC1 <- mean(Input_matrix$PC1)
+  mean_PC2 <- mean(Input_matrix$PC2)
+  
+  row_name <- rownames(Input_matrix)
+  
+  for(i in 1:nrow(Input_matrix)){
+    
+    gene_coor <- as.numeric(Input_matrix[i,1:2])
+    matrix_gene <- rbind(gene_coor, c(mean_PC1, mean_PC2))
+    matrix_gene <- as.numeric(dist(matrix_gene, method = "euclidean"))
+    Input_matrix[i,5] <- matrix_gene
+    
+    
+  }
+  
+  return(as.data.frame(Input_matrix))
   
   
 }
 
+#### --- Usage:
+# PCA_genes %>% group_by(cluster) %>% 
+#   do(f.Centroid.Euclidean.dist(.)) %>% as.data.frame()
 
-
-
-
-
-
-
+#### Good explanation of K-means: https://www.datanovia.com/en/lessons/k-means-clustering-in-r-algorith-and-practical-examples/
 
