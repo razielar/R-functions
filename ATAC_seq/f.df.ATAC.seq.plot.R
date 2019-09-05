df091e27c8903621a4a070659eb0c4e4a926c85a
razielar <razielar@gmail.com>
2019-09-05 14:23:02 +0200

    add ggplot2 commands

--- ATAC_seq/f.df.ATAC.seq.plot.R
+++ ATAC_seq/f.df.ATAC.seq.plot.R
@@ -14,5 +14,17 @@ f.plot.ATAC.seq <- function(input_matrx){
   
 }
 
+### Usage: 
+# intronic_genic <- f.plot.ATAC.seq(input_matrx = intronic_genic)
 
+### Plot: 
+# ggplot(data = intronic_genic, aes(x=Position, y=value, color=variable))+
+#   geom_line()+ggtitle("ATAC-seq: genic-intronic (7) upregulated at 0h")+
+#   ylab("ATAC-seq Read Density at 0h")+xlab("")+
+#   scale_color_manual(values = c("chartreuse4", "orange2"))+
+#   theme(plot.title = element_text(hjust = 0.5, face = "bold"), 
+#         axis.text.y = element_text(face = "bold"),
+#         axis.text.x = element_text(face = "bold"), 
+#         text = element_text(size=10, face = "bold"),
+#         legend.title = element_text(color = "white"))
 
