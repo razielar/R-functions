fc6ba480be2992a20dfae117f53720b4483d2ab0
razielar <razielar@gmail.com>
2019-09-06 12:19:24 +0200

    Modify function in which you remove 2 replicates

--- Regeneration/f.Collapse.Regeneration.replicates.by.mean.R
+++ Regeneration/f.Collapse.Regeneration.replicates.by.mean.R
@@ -2,15 +2,24 @@
 
 ### --- 1) Remove replicates: 'Control-0h-R2' & 'Regeneration-15h-R2'
 f.Collapse_replicates_by.mean.Remove.2replicates <- function(Input_matrix){
-  
   require(dplyr)
   require(stringr)
+  require(magrittr)
   
   col_names <- colnames(Input_matrix)
+  value <- all(str_detect(col_names, ".")) #Detect if has a dot insted of hyphen
+  #If it's true change hyphens for dots
+  if(value){
+    col_names %<>% strsplit(., split=".", fixed=TRUE) %>%
+      lapply(., function(x){y <- x[1:4]; paste(y, collapse="-")}) %>% 
+      unlist()
+  } else{ col_names <- col_names }
   
-  new_colnames <- colnames(Input_matrix) %>% 
+  colnames(Input_matrix) <- col_names
+  
+  new_colnames <- colnames(Input_matrix) %>%
     strsplit(., split="-", fixed=TRUE) %>%
-    lapply(., function(x){y <- x[c(1,2)]; paste(y, collapse="-")}) %>% unlist() 
+    lapply(., function(x){y <- x[c(1,2)]; paste(y, collapse="-")}) %>% unlist()
   
   number_columns <- unique(new_colnames) %>% length()
   
@@ -18,17 +27,15 @@ f.Collapse_replicates_by.mean.Remove.2replicates <- function(Input_matrix){
   colnames(New_df) <- new_colnames %>% unique()
   
   New_df[,1] <- Input_matrix[,1]
-  New_df[,4] <- Input_matrix[,6]
-  
-  New_df[,2] <- apply(Input_matrix[,c(2:3)], 1, mean) %>% round(., digits=2)
-  New_df[,3] <- apply(Input_matrix[,c(4:5)], 1, mean) %>% round(., digits=2)
-  New_df[,5] <- apply(Input_matrix[,c(7:8)], 1, mean) %>% round(., digits=2)
-  New_df[,6] <- apply(Input_matrix[,c(9:10)], 1, mean) %>% round(., digits=2)
+  New_df[,2] <- Input_matrix[,3]
+  New_df[,3] <- apply(Input_matrix[,c(5:6)], 1, mean) %>% round(., digits=2)
+  New_df[,4] <- apply(Input_matrix[,c(7:8)], 1, mean) %>% round(., digits=2)
+  New_df[,5] <- apply(Input_matrix[,c(9:10)], 1, mean) %>% round(., digits=2)
+  New_df[,6] <- apply(Input_matrix[,c(11:12)], 1, mean) %>% round(., digits=2)
   
   rownames(New_df) <- rownames(Input_matrix)
   
   return(New_df)
-  
   
 }
 
@@ -74,5 +81,6 @@ f.Collapse_replicates_by.mean.Remove.ONE.replicates <- function(Input_matrix){
 
 ### Usage: 
 
+# f.Collapse_replicates_by.mean.Remove.2replicates(regeneration)
 # f.Collapse_replicates_by.mean.Remove.ONE.replicates(regeneration) 
 
