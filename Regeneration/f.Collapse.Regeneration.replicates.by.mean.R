e1727679325698581be9857def75449af34adc89
razielar <razielar@gmail.com>
2019-09-06 12:00:48 +0200

    Rearrange the mean of columns

--- Regeneration/f.Collapse.Regeneration.replicates.by.mean.R
+++ Regeneration/f.Collapse.Regeneration.replicates.by.mean.R
@@ -60,24 +60,22 @@ f.Collapse_replicates_by.mean.Remove.ONE.replicates <- function(Input_matrix){
   colnames(New_df) <- new_colnames %>% unique()
 
   New_df[,1] <- Input_matrix[,1]
-
-  New_df[,2] <- apply(Input_matrix[,c(2:3)], 1, mean) %>% round(., digits=2)
-  New_df[,3] <- apply(Input_matrix[,c(4:5)], 1, mean) %>% round(., digits=2)
-  New_df[,4] <- apply(Input_matrix[,c(6:7)], 1, mean) %>% round(., digits=2)
-  New_df[,5] <- apply(Input_matrix[,c(8:9)], 1, mean) %>% round(., digits=2)
-  New_df[,6] <- apply(Input_matrix[,c(10:11)], 1, mean) %>% round(., digits=2)
+  New_df[,2] <- apply(Input_matrix[,c(3:4)], 1, mean) %>% round(., digits=2)
+  New_df[,3] <- apply(Input_matrix[,c(5:6)], 1, mean) %>% round(., digits=2)
+  New_df[,4] <- apply(Input_matrix[,c(7:8)], 1, mean) %>% round(., digits=2)
+  New_df[,5] <- apply(Input_matrix[,c(9:10)], 1, mean) %>% round(., digits=2)
+  New_df[,6] <- apply(Input_matrix[,c(11:12)], 1, mean) %>% round(., digits=2)
 
   rownames(New_df) <- rownames(Input_matrix)
 
   return(New_df)
-  
   
 }
 
 
 ### Usage: 
 
-# f.Collapse_replicates_by.mean.Remove.ONE.replicates(regeneration)
+# f.Collapse_replicates_by.mean.Remove.ONE.replicates(regeneration) 
 
 
 
