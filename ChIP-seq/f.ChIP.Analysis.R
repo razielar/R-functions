d37311e30d0b7de8fd1e602503ab50ef74ac4433
razielar <razielar@gmail.com>
2019-09-11 16:17:00 +0200

    Add format on bwtool command

--- ChIP-seq/f.ChIP.Analysis.R
+++ ChIP-seq/f.ChIP.Analysis.R
@@ -4,30 +4,29 @@
 ### This function is not longer useful it was when I didn't know how to use bwtool:
 ###### 1) Generate the df for ploting: the order must be: "RNApol", "H3K4me1", "H3K27ac"
 # f.Plot_ChIP_seq <- function(Input_1, Input_2, Input_3){
-#   
 #   require(reshape2)
 #   require(dplyr)
-#   
 #   colnames(Input_1)[1] <- "V1"; colnames(Input_2)[1] <- "V1"; colnames(Input_3)[1] <- "V1"
-#   
 #   tmp_plot <- merge(Input_1, Input_2, by.x= "V1", by.y="V1") %>% 
 #     merge(., Input_3, by.x= "V1", by.y="V1")
-#   
 #   colnames(tmp_plot) <- c("Position", "RNApol", "H3K4me1", "H3K27ac")
-#   
 #   plot_control <- melt(tmp_plot , id="Position") 
 #   colnames(plot_control)[2] <- "Chromatin_Mark"
-#   
 #   return(plot_control)
-#   
-#   
 # }
 
 ### --- bwtool command: 
 # bwtool aggregate up:down bed_file.txt \
-# NIH3K4m1_REGENERATION_quantile-normalized.bw,NIRNApol_REGENERATION_quantile-normalized.bw,NIH3K27ac_REGENERATION_quantile-normalized.bw \
+# NIH3K4m1_quantile-normalized.bw,NIRNApol_quantile-normalized.bw,NIH3K27ac_quantile-normalized.bw \
 # /dev/stdout -starts -header > output
 
+###### 1) Generate the df for the 3 Histone Marks: 
+      ### Generate one plot with one condition (Control or Regeneration) and 3 histone marks
+      ### Order: H3K4m1, RNApol, H3K27ac
+
+f.Plot_ChIP_3HistoneMarks_Condition <- function(input_matrix){
+  
+}
 
 
 ####### 2) Generate the df for plotting in one plot a Histone mark for Control_Up_0h, 
