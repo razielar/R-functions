60f967dcd82f263c5f354f63e4012313fb993298
razielar <razielar@gmail.com>
2019-09-11 16:06:19 +0200

    comment the first function

--- ChIP-seq/f.ChIP.Analysis.R
+++ ChIP-seq/f.ChIP.Analysis.R
@@ -1,26 +1,30 @@
-################### R-functions to plot ChIP-seq signals 
+################### R-functions to plot ChIP-seq signals: 
+### September 11th 2019
 
+### This function is not longer useful it was when I didn't know how to use bwtool:
 ###### 1) Generate the df for ploting: the order must be: "RNApol", "H3K4me1", "H3K27ac"
+# f.Plot_ChIP_seq <- function(Input_1, Input_2, Input_3){
+#   
+#   require(reshape2)
+#   require(dplyr)
+#   
+#   colnames(Input_1)[1] <- "V1"; colnames(Input_2)[1] <- "V1"; colnames(Input_3)[1] <- "V1"
+#   
+#   tmp_plot <- merge(Input_1, Input_2, by.x= "V1", by.y="V1") %>% 
+#     merge(., Input_3, by.x= "V1", by.y="V1")
+#   
+#   colnames(tmp_plot) <- c("Position", "RNApol", "H3K4me1", "H3K27ac")
+#   
+#   plot_control <- melt(tmp_plot , id="Position") 
+#   colnames(plot_control)[2] <- "Chromatin_Mark"
+#   
+#   return(plot_control)
+#   
+#   
+# }
 
-f.Plot_ChIP_seq <- function(Input_1, Input_2, Input_3){
-  
-  require(reshape2)
-  require(dplyr)
-  
-  colnames(Input_1)[1] <- "V1"; colnames(Input_2)[1] <- "V1"; colnames(Input_3)[1] <- "V1"
-  
-  tmp_plot <- merge(Input_1, Input_2, by.x= "V1", by.y="V1") %>% 
-    merge(., Input_3, by.x= "V1", by.y="V1")
-  
-  colnames(tmp_plot) <- c("Position", "RNApol", "H3K4me1", "H3K27ac")
-  
-  plot_control <- melt(tmp_plot , id="Position") 
-  colnames(plot_control)[2] <- "Chromatin_Mark"
-  
-  return(plot_control)
-  
-  
-}
+
+
 
 ####### 2) Generate the df for plotting in one plot a Histone mark for Control_Up_0h, 
       #### Control_not_Up_0h, Regeneration_Up_0h, Regeneration_not_Up_0h
