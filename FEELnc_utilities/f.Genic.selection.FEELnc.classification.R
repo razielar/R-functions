a5ebccae6577400a702f787ec00212325ab41ccb
razielar <razielar@gmail.com>
2019-09-17 18:53:18 +0200

    Add the final function

--- FEELnc_utilities/f.Genic.selection.FEELnc.classification.R
+++ FEELnc_utilities/f.Genic.selection.FEELnc.classification.R
@@ -24,32 +24,31 @@ f.select_genic <- function(Input_matrix){
   return(Genic)
 }
 
+f.Keep.unique.genic <- function(Input_matrix){
+  
+  Input_matrix <- as.data.frame(Input_matrix)
+  tmp_df <- Input_matrix[Input_matrix$isBest == 1 & Input_matrix$type == "genic",]
+  partner_mRNA <- unique(tmp_df$partnerRNA_gene)
+  
+  polish_genic <- NULL
+  
+  for(i in 1:length(partner_mRNA)){
+    
+    selected <- tmp_df[tmp_df$partnerRNA_gene %in% partner_mRNA[i],][1,]
+    polish_genic <- rbind(polish_genic, selected)
+    
+  }
+  return(polish_genic)
+}
+
+genic %<>% group_by(lncRNA_gene) %>% do(f.Keep.unique.genic(.)) %>%
+  as.data.frame() 
 
 
 
 
-# f.Find.antisense.multiple.genes <- function(Input_matrix){
-#   
-#   Input_matrix <- as.data.frame(Input_matrix)
-#   Antisense <- c(); TMP_final <- c(); Final_value <- c() #Declare initial variables
-#   
-#   if(length(unique(Input_matrix[,4])) == 1){ 
-#     
-#     Antisense <- Input_matrix[1,] #If the gene repeats several times grab the first one
-#     
-#   }  else{
-#     
-#     unique_values <- unique(Input_matrix[,4]) #If not grab the unique mRNA pairs
-#     
-#     for(i in 1:length(unique_values)){
-#       
-#       tmp <- Input_matrix[Input_matrix[,4] == unique_values[i],][1,]
-#       TMP_final <- rbind(TMP_final, tmp)
-#       
-#     }
-#   }
-#   
-#   Final_value <- rbind(Antisense, TMP_final)
-#   return(as.data.frame(Final_value))
-#   
-# }
+
+
+
+
+
