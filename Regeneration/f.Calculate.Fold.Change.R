fa77e0779a3f5b34656f5618bda8dbd9cf26df99
razielar <razielar@gmail.com>
2019-10-18 12:33:47 +0200

    Add function to add 1 TPM to all values for FoldChange calculation

--- Regeneration/f.Calculate.Fold.Change.R
+++ Regeneration/f.Calculate.Fold.Change.R
@@ -1,6 +1,6 @@
 ##### Calculate Fold change Funcitons:
 
-########### 1) Calculate Fold change:
+########### 1.1) Calculate Fold change:
 ### October 14th 2019
 ### Instructions:
 # The input matrix MUST have the following order:
@@ -89,6 +89,82 @@ f.FoldChange <- function(input_matrix, psuedo_count=0.0000001){
 
 ### Usage: 
 ## tmp <-  f.FoldChange(input_matrix = expression)
+
+########### 1.2) Calculate Fold change:
+### October 17th 2019
+### Description:
+# In order to avoid biases we added 1 to all the samples to calculate the FoldChange
+# in all the ranges from 0 to <1 TPMs 
+### Instructions:
+# The input matrix MUST have the following order:
+# "Control-0h" "Regeneration-0h" "Control-15h" "Regeneration-15h"
+# "Control-25h" "Regeneration-25h"
+### R_script: /nfs/users2/rg/ramador/D_me/RNA-seq/DGE_reanalysis/dme_r6.29/R_scripts/Fold.change.TPM.add.1.comparison.R
+
+f.FoldChange.AddOne <- function(input_matrix){
+
+    require(gtools, dplyr)
+    
+    output <- data.frame(matrix(nrow=nrow(input_matrix), ncol=12))
+    rownames(output) <- rownames(input_matrix)
+    colnames(output) <- c(colnames(input_matrix)[1:2],
+                          "FoldChange_0h","FoldChange_0h_group",
+                          colnames(input_matrix)[3:4],
+                          "FoldChange_15h","FoldChange_15h_group",
+                          colnames(input_matrix)[5:6],
+                          "FoldChange_25h","FoldChange_25h_group" )    
+    
+    for(i in 1:nrow(input_matrix)){
+
+        fold_early <- foldchange(input_matrix[i,2]+1,input_matrix[i,1]+1)
+        fold_mild <- foldchange(input_matrix[i,4]+1,input_matrix[i,3]+1)
+        fold_late <- foldchange(input_matrix[i,6]+1,input_matrix[i,5]+1)
+
+        output[i,3] <- fold_early %>% round(., digits = 3)
+        output[i,7] <- fold_mild %>% round(., digits = 3)
+        output[i,11] <- fold_late %>% round(., digits = 3)
+
+        if(fold_early >= 1.69){
+            output[i,4] <- "upregulated"
+        } else if(fold_early <= -1.69){
+            output[i,4] <- "downregulated"
+        } else{
+            output[i,4] <- "flat"
+        }
+
+        if(fold_mild >= 1.69){
+            output[i,8] <- "upregulated"
+        } else if(fold_mild <= -1.69){
+            output[i,8] <- "downregulated"
+        } else{
+            output[i,8] <- "flat"
+        } 
+
+        if(fold_late >= 1.69){
+            output[i,12] <- "upregulated"
+        } else if(fold_late <= -1.69){
+            output[i,12] <- "downregulated"
+        } else{
+            output[i,12] <- "flat"
+        }           
+    }
+
+    output[,1] <- input_matrix[,1] %>% round(., digits = 3)
+    output[,2] <- input_matrix[,2] %>% round(., digits = 3)
+    output[,5] <- input_matrix[,3] %>% round(., digits = 3)
+    output[,6] <- input_matrix[,4] %>% round(., digits = 3)
+    output[,9] <- input_matrix[,5] %>% round(., digits = 3)
+    output[,10] <- input_matrix[,6] %>% round(., digits = 3)
+
+    return(output)
+}
+
+### Usage:
+## tmp <- f.FoldChange.AddOne(input_matrix = expression)
+
+
+## tmp <-  f.FoldChange(input_matrix = expression)
+
 
 ########### 2) Function to get ride of low expressed genes labeled as DGE
 ### October 15th 2019
