6efedbd791987b94b9be84e1093e7d8c81582c7f
razielar <razielar@gmail.com>
2019-10-31 11:01:19 +0100

    Add description, ggplot commands and plot example

--- f.Generate.time.point.plot.R
+++ f.Generate.time.point.plot.R
@@ -1,33 +1,80 @@
-library(reshape2)
-library(ggplot2)
+############ Functions to do some plotting 
 
+### --- 1) Comparison of Control and Regeneration throught time-points
+
+## Input matrix:
+##        Control-0h Regeneration-0h Control-15h Regeneration-15h Control-25h
+## cbt          4.60            7.19        6.67             8.27        7.36
+## Gadd45      12.06           39.48       20.41            29.85       25.49
+## yki         16.67           27.38       18.77            40.66       28.62
+## Mmp1        10.29           13.35       17.77            14.57       12.94
+## rpr          9.54           83.86       21.27            18.66       25.51
+##        Regeneration-25h
+## cbt                7.79
+## Gadd45            18.05
+## yki               34.06
+## Mmp1              12.50
+## rpr               11.44
+
+## Example of plot: https://public-docs.crg.es/rguigo/Data/ramador/dme/RNA-seq/dm6-r6.29/Control.genes.dm6.r6.29.pdf
 
 f.Create.line.points <- function(Input_matrix){
-  
-  Initial_df <- Input_matrix
-  number_genes <- nrow(Initial_df)
-  gene_names <- rownames(Initial_df)
-  Final_df <- c()
-  
-  for(i in 1:number_genes){
+
+    require(dplyr, reshape2, ggplot2)
     
-    tmp_melt <- melt(Initial_df[i,])
-    tmp_melt$time_point <- tmp_melt$variable %>% as.character() %>%
-      strsplit(., split="-", fixed=TRUE) %>% lapply(., function(x){y <- x[2]}) %>%
-      unlist()
+    Initial_df <- Input_matrix
+    number_genes <- nrow(Initial_df)
+    gene_names <- rownames(Initial_df)
+    Final_df <- c()
     
-    tmp_melt$Treatment <- tmp_melt$variable %>% as.character() %>%
-      strsplit(., split="-", fixed=TRUE) %>% lapply(., function(x){y <- x[1]}) %>%
-      unlist()
+    for(i in 1:number_genes){
+        
+        tmp_melt <- melt(Initial_df[i,])
+        tmp_melt$time_point <- tmp_melt$variable %>% as.character() %>%
+            strsplit(., split="-", fixed=TRUE) %>% lapply(., function(x){y <- x[2]}) %>%
+            unlist()
+        
+        tmp_melt$Treatment <- tmp_melt$variable %>% as.character() %>%
+            strsplit(., split="-", fixed=TRUE) %>% lapply(., function(x){y <- x[1]}) %>%
+            unlist()
+        
+        tmp_melt$Gene_label <- rep(gene_names[i], nrow(tmp_melt))
+        Final_df <- rbind(Final_df, tmp_melt)
+        
+    }
     
-    tmp_melt$Gene_label <- rep(gene_names[i], nrow(tmp_melt))
-    Final_df <- rbind(Final_df, tmp_melt)
+    return(Final_df)
     
-  }
-  
-  return(Final_df)
-  
 }
+
+### Output from the function:
+##            variable value time_point    Treatment Gene_label
+## 1        Control-0h  4.60         0h      Control        cbt
+## 2   Regeneration-0h  7.19         0h Regeneration        cbt
+## 3       Control-15h  6.67        15h      Control        cbt
+## 4  Regeneration-15h  8.27        15h Regeneration        cbt
+## 5       Control-25h  7.36        25h      Control        cbt
+## 6  Regeneration-25h  7.79        25h Regeneration        cbt
+## 7        Control-0h 12.06         0h      Control     Gadd45
+## 8   Regeneration-0h 39.48         0h Regeneration     Gadd45
+## 9       Control-15h 20.41        15h      Control     Gadd45
+## 10 Regeneration-15h 29.85        15h Regeneration     Gadd45
+
+### ggplot commands:
+
+## ggplot(data = Data_Plot, aes(x=time_point, y=value, group=Treatment))+
+##     geom_line(size=2, aes(color=Treatment))+
+##     geom_point(size=3.5, aes(color=Treatment))+
+##     facet_wrap(~Gene_label, ncol=2, scales="free")+
+##     xlab("")+ylab("TPM")+
+##     theme(axis.text.y = element_text(face = "bold"),
+##           text = element_text(size=16, face = "bold"),
+##           axis.text.x = element_text(face = "bold"),
+##           axis.title.y = element_text(face="bold"),
+##         strip.text.x = element_text(size = 16), legend.position="bottom")+
+##   scale_color_manual(values = c("olivedrab", "orange2"))
+
+
 
 f.Rearrange_labels <- function(Input_matrix){
   
@@ -97,11 +144,6 @@ f.Time_Point_lncRNAmRNA_pairs <- function(Expression_matrix, mRNA_lncRNA_pairs){
   
   
 }
-
-
-
-
-
 
 
 
