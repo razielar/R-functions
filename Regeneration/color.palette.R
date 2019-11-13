08077bf4676b4b31b7495d72cbcb457be93e1074
razielar <razielar@gmail.com>
2019-11-13 18:03:38 +0100

    add color palette

---
+++ Regeneration/color.palette.R
@@ -0,0 +1,20 @@
+### Color palette for regeneration study
+### November 13th 2019
+
+### --- DGE:
+dge_flat <- "darkgray"
+dge_up <- "firebrick2"      ##"firebrick1"
+dge_down <- "dodgerblue2"
+
+### --- lncRNA/mRNA:
+
+### --- lncRNA subtypes: intergenic, genic-exonic, genic-intronic
+lncRNA_subtypes_intergenic <- "chocolate2"
+lncRNA_subtypes_genic_exonic <- "darkorange4"
+lncRNA_subtypes_genic_intronic <- "darkgoldenrod"
+
+### --- Control vs. Regeneration: 
+control_sample <- "olivedrab"
+regene_sample <- "orange2"
+
+
