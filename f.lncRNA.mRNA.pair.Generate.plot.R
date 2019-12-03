214d30f90cc246ae249fbe3b9cd13989266123af
razielar <razielar@gmail.com>
2019-12-03 18:44:33 +0100

    add error handling

--- f.lncRNA.mRNA.pair.Generate.plot.R
+++ f.lncRNA.mRNA.pair.Generate.plot.R
@@ -14,28 +14,49 @@ f.lncRNA.mRNA.pair <- function(expression_matrix, lncRNA_mRNA){
 
       cat(i, "\n")
       gene_pairs <- lncRNA_mRNA[i,c(1,3)] %>% as.character()
+
+      print(paste("lncRNA: ",gene_pairs[1], "mRNA: ", gene_pairs[2]))
+      
       df_plot <- expression_matrix[which(rownames(expression_matrix) %in%
                                          gene_pairs),]
-      df_plot <- melt(t(df_plot))
-      
-      ## Add GeneName:
-    
-      gene_name_pairs <- lncRNA_mRNA[i,c(2,4)] %>% as.character()
-      df_plot$Gene <- c(rep(gene_name_pairs[2], nrow(df_plot)/2),
-                        rep(gene_name_pairs[1], nrow(df_plot)/2))
-    
-      ### mRNA always first in factors
-      df_plot$Gene <- as.factor(df_plot$Gene)
-      
-      if( as.character(levels(df_plot$Gene)[1]) == gene_name_pairs[2]  ){
-          df_plot$Gene <- factor(df_plot$Gene,
-                                 levels = levels(df_plot$Gene)[c(1,2)])
-      } else {
-          df_plot$Gene <- factor(df_plot$Gene,
-                                 levels = levels(df_plot$Gene)[c(2,1)])
-        }
-      
-      df_list[[i]] <- df_plot
+
+      if(nrow(df_plot) == 0){
+          
+          rlang::warn(message = "Both lncRNA and mRNA are not present",
+                      .subclass = "both missing")
+          
+      } else if(nrow(df_plot) == 1){
+          
+          rlang::warn(message = "The Dataframe should have two values",
+                      .subclass = "one missing value")
+          
+          df_plot <- melt(t(df_plot))
+          
+          df_plot$Gene <- rep("Something" , nrow(df_plot)) #IMPROVE THIS!!!!
+          df_list[[i]] <- df_plot
+          
+      } else{
+          df_plot <- melt(t(df_plot))
+          
+          ## Add GeneName:          
+          gene_name_pairs <- lncRNA_mRNA[i,c(2,4)] %>% as.character()
+          df_plot$Gene <- c(rep(gene_name_pairs[2], nrow(df_plot)/2),
+                            rep(gene_name_pairs[1], nrow(df_plot)/2))
+          
+          ### mRNA always first in factors
+          df_plot$Gene <- as.factor(df_plot$Gene)
+          
+          if( as.character(levels(df_plot$Gene)[1]) == gene_name_pairs[2]  ){
+              df_plot$Gene <- factor(df_plot$Gene,
+                                     levels = levels(df_plot$Gene)[c(1,2)])
+          } else {
+              df_plot$Gene <- factor(df_plot$Gene,
+                                     levels = levels(df_plot$Gene)[c(2,1)])
+          }
+          
+          df_list[[i]] <- df_plot
+
+      }
     
   }
   
