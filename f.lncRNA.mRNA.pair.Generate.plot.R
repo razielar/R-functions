c78ffcdd5647c1e071ef206cda621952d8a1ec7a
razielar <razielar@gmail.com>
2019-12-03 16:03:55 +0100

    change variable name from df_pair_plot to lncRNA_mRNA; generalized script

--- f.lncRNA.mRNA.pair.Generate.plot.R
+++ f.lncRNA.mRNA.pair.Generate.plot.R
@@ -10,31 +10,37 @@ f.lncRNA.mRNA.pair <- function(expression_matrix, lncRNA_mRNA){
   
   df_list <- list()
   
-  for(i in 1:nrow(lncRNA_phenotype)){
+  for(i in 1:nrow(lncRNA_mRNA)){
+
+      cat(i, "\n")
+      gene_pairs <- lncRNA_mRNA[i,c(1,3)] %>% as.character()
+      df_plot <- expression_matrix[which(rownames(expression_matrix) %in%
+                                         gene_pairs),]
+      df_plot <- melt(t(df_plot))
+      
+      ## Add GeneName:
     
-    gene_pairs <- df_pair_plot[i,c(1,3)] %>% as.character()
-    df_plot <- expression_matrix[which(rownames(expression_matrix) %in% gene_pairs),]
-    df_plot <- melt(t(df_plot))
-    ### Add GeneName:
+      gene_name_pairs <- lncRNA_mRNA[i,c(2,4)] %>% as.character()
+      df_plot$Gene <- c(rep(gene_name_pairs[2], nrow(df_plot)/2),
+                        rep(gene_name_pairs[1], nrow(df_plot)/2))
     
-    gene_name_pairs <- df_pair_plot[i,c(2,4)] %>% as.character()
-    df_plot$Gene <- c(rep(gene_name_pairs[2], nrow(df_plot)/2),
-                      rep(gene_name_pairs[1], nrow(df_plot)/2))
-    
-    ### mRNA always first in factors
-    df_plot$Gene <- as.factor(df_plot$Gene)
-    
-    if( as.character(levels(df_plot$Gene)[1]) == gene_name_pairs[2]  ){
-      df_plot$Gene <- factor(df_plot$Gene, levels = levels(df_plot$Gene)[c(1,2)])
-    } else {
-      df_plot$Gene <- factor(df_plot$Gene, levels = levels(df_plot$Gene)[c(2,1)])
-      }
-   
-    df_list[[i]] <- df_plot
+      ### mRNA always first in factors
+      df_plot$Gene <- as.factor(df_plot$Gene)
+      
+      if( as.character(levels(df_plot$Gene)[1]) == gene_name_pairs[2]  ){
+          df_plot$Gene <- factor(df_plot$Gene,
+                                 levels = levels(df_plot$Gene)[c(1,2)])
+      } else {
+          df_plot$Gene <- factor(df_plot$Gene,
+                                 levels = levels(df_plot$Gene)[c(2,1)])
+        }
+      
+      df_list[[i]] <- df_plot
     
   }
   
   return(df_list)
+  
 }
 
 ### Usage: 
