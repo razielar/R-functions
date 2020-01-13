7d32dff2b6632918a12ce21cff2755dbbe74f5cd
razielar <razielar@gmail.com>
2020-01-13 17:45:18 +0100

    add ortholog analysis with bioMaRt

--- bioMart.example.dme.R
+++ bioMart.example.dme.R
@@ -59,5 +59,58 @@ datProbes <- getBM(attributes = getinfo, filters=c("ensembl_gene_id"),
 ## datX0h <- getBM(attributes = getinfo, filters=c("external_gene_name"),
 ##                    values= X0h$Gene ,mart=mart) 
 
+##############################################################################################
+###### Orthologs analysis: Zebra fish, mouse and human 
+
+library(tidyverse)
+library(magrittr)
+library(biomaRt)
+setwd("/users/rg/ramador/D_me/Data/Genes/dm6_r6.29")
+options(stringsAsFactors = F)
+
+## information <- listAttributes(mart = mart)
+## information$page %>% table
+## information %>% filter(page == "homologs") %>% filter(description %in%
+##                                                       c("Zebrafish gene stable ID"))
+
+ensembl <- useEnsembl(biomart = "ensembl", 
+                      dataset = "dmelanogaster_gene_ensembl",
+                      host = "jan2019.archive.ensembl.org")
+
+info <- c("ensembl_gene_id", "external_gene_name", "chromosome_name", "description",
+          "drerio_homolog_ensembl_gene","drerio_homolog_associated_gene_name",
+          "mmusculus_homolog_ensembl_gene", "mmusculus_homolog_associated_gene_name",
+          "hsapiens_homolog_ensembl_gene","hsapiens_homolog_associated_gene_name")
+
+extra_info <- getBM(mart = ensembl, filters = "ensembl_gene_id",
+      values = datExpr$Gene_ID,
+      attributes = info)
+
+## extra_info$ensembl_gene_id %>% unique %>% length
+
+extra_info %<>%
+        arrange(chromosome_name, external_gene_name)
+
+
+colnames(extra_info)[c(1:3)] <- c("Gene_ID", "Gene_Name", "Chr")
+colnames(extra_info) <- colnames(extra_info) %>% str_to_title
+
+
+prev_info <- read_tsv("bioMaRt//Results//GeneID.Name.Type.Length.Description.GC.tsv")
+prev_info %<>% dplyr::select(Gene_id, Gene_biotype) 
+
+
+## Obtain Gene_biotype:
+extra_info %<>% left_join(., prev_info, by = "Gene_id") %>%
+    dplyr::select(Gene_id:Description, Gene_biotype,
+                  Drerio_homolog_ensembl_gene:Hsapiens_homolog_associated_gene_name) 
+
+extra_info$Description  %<>% strsplit(., " ", fixed = TRUE) %>%
+    lapply(., function(x){y <- x[1:length(x)]; paste(y, collapse = "_")}) %>%
+    unlist 
+
+## write.table(extra_info, file = "bioMaRt//Results//GeneID.Name.Type.Description.Orthologs.tsv",
+##             sep = "\t", quote = F, col.names = T, row.names = F)
+
 
 
