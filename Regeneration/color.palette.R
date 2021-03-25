758505d65fc2aa4f08ce68f34300dc70644d7018
razielar <razielar@gmail.com>
2021-03-25 10:13:16 +0100

    change from r colors to Hex

--- Regeneration/color.palette.R
+++ Regeneration/color.palette.R
@@ -3,36 +3,35 @@
 
 ### --- DGE:
 dge_flat <- "darkgray"
-dge_up <- "firebrick2"      ##"firebrick1"
-dge_down <- "dodgerblue2"
+dge_up <- "#ee2c2c" ##firebrick2
+dge_down <- "#1c86ee" ##dodgerblue2
 
 # For special occasions:
-dge_extreme_up <- "firebrick"
-dge_extreme_down <- "dodgerblue4"
+dge_extreme_up <- "#b22222" ##firebrick
+dge_extreme_down <- "#104e8b" ##dodgerblue4
     
 ### --- lncRNA/mRNA:
-lncRNA_biotype <- "aquamarine4"
-mRNA_biotype <- "azure4"
+lncRNA_biotype <- "#458b74" ##aquamarine4
+mRNA_biotype <- "#838b8b" ##azure4
 
 ### --- lncRNA subtypes: intergenic, genic-exonic, genic-intronic
-lncRNA_subtypes_intergenic <- "chocolate2"
-lncRNA_subtypes_genic_exonic <- "darkorange4"
-lncRNA_subtypes_genic_intronic <- "darkgoldenrod"
+lncRNA_subtypes_intergenic <- "#ee7621" ##chocolate2
+lncRNA_subtypes_genic_exonic <- "#8b4500" ##darkorange4
+lncRNA_subtypes_genic_intronic <- "#b8860b" ##darkgoldenrod
 
 ### --- Control vs. Regeneration: 
-l3 <- "antiquewhite"
-control_sample <- "olivedrab"
-regene_sample <- "orange2"
+l3 <- "#faebd7" ##antiquewhite
+control_sample <- "#6b8e23" ##olivedrab
+regene_sample <- "#ee9a00" ##orange2
 
 ### --- Time-Points:
-early <- "snow3"
-mid <- "snow4"
-late <- "gray24"
+early <- "#cdc9c9" ##snow3
+mid <- "#8b8989" ##snow4
+late <- "#3d3d3d" ##gray24
 
 ### --- Neighbor PCGs:
-concordant_col <- "slateblue4"
-discordant_col <- "slateblue1"
-ne_concordant_col <- "gray70"
-    
+concordant_col <- "#473c8b" ##slateblue4
+discordant_col <- "#836fff" ##slateblue1
+ne_concordant_col <- "#b3b3b3" ##gray70
 
 
