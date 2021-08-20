59b9fc5eb624a675eb8c8772b2dba37c586500d3
razielar <razielar@gmail.com>
2021-08-20 10:59:54 +0200

    add ml color scheme

---
+++ ml/colorScheme.R
@@ -0,0 +1,20 @@
+#### COLOR SCHEME for ML project
+
+### Hit colors: 
+not_hit_color <- "#9e9e9e"
+hit_color <- "#cc0000"
+
+## hit_colors= {"not_hit": "#9e9e9e", "hit": "#cc0000"}
+
+### Cell-colors:
+color_ipsc <- "#ee6a50"
+color_k562 <- "#8a8a8a"
+color_u87 <- "#caff70"
+color_mcf7 <- "#cd853f"
+color_hela <- "#87ceeb"
+color_mda <- "#fff68f"
+color_hek <- "#007e00"
+
+## cell_colors= {"ipsc": "#ee6a50", "k562": "#8a8a8a", "u87": "#caff70", "mcf7": "#cd853f",
+##     "hela": "#87ceeb", "mda": "#fff68f", "hek": "#007e00"}
+
